48a673c2b300651f166406f79d3d6d255620c4fb
mohan4337 <mmenggservice@gmail.com>
2026-03-11 18:09:43 +0530

    fix anchor href error

--- sun-emirates-frontend/src/pages/AboutUs.js
+++ sun-emirates-frontend/src/pages/AboutUs.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import SpecialistAreas from '../components/SpecialistAreas';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import SpecialistAreas from "../components/SpecialistAreas";
 
 const AboutUs = () => {
-  const [activeSection, setActiveSection] = useState(0);
+  const [activeSection, setActiveSection] = useState(-1);
 
   const sections = [
     {
-      title: 'Company Information',
+      title: "Company Information",
       content: (
-        <table cellpadding="0" cellspacing="0" border="0" width="99%" className="tablestr">
+        <table cellPadding="0" cellSpacing="0" border="0" width="99%" className="tablestr">
           <tbody>
             <tr>
               <td className="abtLabel">Year of Establishment</td>
@@ -32,9 +32,9 @@ const AboutUs = () => {
       )
     },
     {
-      title: 'Company Location Information',
+      title: "Company Location Information",
       content: (
-        <table cellpadding="0" cellspacing="0" border="0" width="99%" className="tablestr">
+        <table cellPadding="0" cellSpacing="0" border="0" width="99%" className="tablestr">
           <tbody>
             <tr>
               <td className="abtLabel">Total Area</td>
@@ -53,9 +53,9 @@ const AboutUs = () => {
       )
     },
     {
-      title: 'Company Employees Information',
+      title: "Company Employees Information",
       content: (
-        <table cellpadding="0" cellspacing="0" border="0" width="99%" className="tablestr">
+        <table cellPadding="0" cellSpacing="0" border="0" width="99%" className="tablestr">
           <tbody>
             <tr>
               <th className="abtLabel">Number of Employees</th>
@@ -78,11 +78,11 @@ const AboutUs = () => {
               <td className="abtLabel">02</td>
             </tr>
             <tr>
-              <td className="abtLabel">Fabricators(Fittter)</td>
+              <td className="abtLabel">Fabricators (Fitter)</td>
               <td className="abtLabel">07</td>
             </tr>
             <tr>
-              <td className="abtLabel">Welders(Blaster & Painter)</td>
+              <td className="abtLabel">Welders (Blaster & Painter)</td>
               <td className="abtLabel">04</td>
             </tr>
             <tr>
@@ -112,74 +112,83 @@ const AboutUs = () => {
   };
 
   return (
-    <table cellpadding="0" border="0" cellspacing="0" width="100%">
+    <table cellPadding="0" border="0" cellSpacing="0" width="100%">
       <tbody>
         <tr>
           <td width="70%">
-            <table cellpadding="0" border="0" cellspacing="0" width="100%">
+            <table cellPadding="0" border="0" cellSpacing="0" width="100%">
               <tbody>
                 <tr>
                   <td align="center">
-                    <div className="PageHeader">
-                      About Us
-                    </div>
-                    <table cellpadding="0" cellspacing="0" width="100%">
+                    <div className="PageHeader">About Us</div>
+
+                    <table cellPadding="0" cellSpacing="0" width="100%">
                       <tbody>
                         <tr>
-                          <td align="center" colSpan={2}>
-                            <img 
-                              src="/images/Common/Aboutsbnr.png" 
-                              alt="About Us" 
+                          <td align="center" colSpan="2">
+                            <img
+                              src="/images/Common/Aboutsbnr.png"
+                              alt="About Us"
                               title="About Us"
-                              align="absMiddle"
+                              style={{ maxWidth: "100%" }}
                             />
                           </td>
                         </tr>
                       </tbody>
                     </table>
+
                     <div className="abtContentsView">
                       <p className="abtparp">
-                        We are pleased to introduce ourselves as <b>SUN EMIRATES MECHANICAL WORKS LLC</b>,
-                        expertise in Steel structural designers and contractors and one of the growing steel
-                        fabrication company located in Al Shenaya New Industrial area , Ajman, U.A.E established
-                        in 2007. We have been in the business of stainless steel and mild steel fabrication
-                        for Construction, Interior Decoration, Marine, Power and Oil & Gas sectors.
+                        We are pleased to introduce ourselves as{" "}
+                        <b>SUN EMIRATES MECHANICAL WORKS LLC</b>, expertise in
+                        Steel structural designers and contractors and one of
+                        the growing steel fabrication company located in Al
+                        Shenaya New Industrial area, Ajman, U.A.E established in
+                        2007.
                       </p>
+
                       <p className="abtparp">
-                        As a multi-faceted establishment, we are continually growing, diversifying into
-                        new product areas, improving on the existing ones with best quality on time and
-                        servicing the ever-growing multi industrial sectors in the Gulf.
+                        As a multi-faceted establishment, we are continually
+                        growing, diversifying into new product areas, improving
+                        existing ones with best quality and on-time delivery.
                       </p>
+
                       <p className="abtparp">
-                        You are important to us, we therefore feel the need to register our company in your
-                        esteemed organization and kindly send us your valuable enquiries. We can offer you
-                        the best competitive price with best delivery. We are herewith attaching our company
-                        pre-qualification documents for your records.
+                        You are important to us, we therefore feel the need to
+                        register our company in your esteemed organization and
+                        kindly send us your valuable enquiries.
                       </p>
+
                       <p className="abtparp">
-                        Assuring our best attention , Services and healthy business relation at all times.
+                        Assuring our best attention, services and healthy
+                        business relation at all times.
                       </p>
+
                       <p className="abtparp">
                         Awaiting to receive your valued enquiries.
                         <Link to="/feedback"> Post your valuable queries</Link>
                       </p>
+
                       <p className="abtparp">
-                        We have experienced technical team who expertise on producing ranges of products
-                        in time with best quality to meet with the requirement and specification of the
-                        clients.
+                        We have experienced technical team who expertise on
+                        producing ranges of products in time with best quality.
                       </p>
+
                       <br />
                     </div>
+
                     <div className="accordinAbtUs">
                       {sections.map((section, index) => (
                         <div key={index}>
-                          <h3 onClick={() => toggleSection(index)} style={{ cursor: 'pointer' }}>
-                            <a href="#">{section.title}</a>
+                          <h3
+                            onClick={() => toggleSection(index)}
+                            style={{ cursor: "pointer" }}
+                          >
+                            {section.title}
                           </h3>
+
                           {activeSection === index && (
-                            <div>
-                              {section.content}
-                            </div>
+                            <div>{section.content}</div>
                           )}
                         </div>
                       ))}
@@ -189,14 +198,14 @@ const AboutUs = () => {
               </tbody>
             </table>
           </td>
+
           <td width="30%" valign="top" className="osa">
             <SpecialistAreas />
           </td>
         </tr>
+
         <tr>
-          <td style={{ height: '25px' }}>
-            &nbsp;
-          </td>
+          <td style={{ height: "25px" }}>&nbsp;</td>
         </tr>
       </tbody>
     </table>
